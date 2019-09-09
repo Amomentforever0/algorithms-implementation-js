b5e5f2a03edba49afb6234fdbfaacf3a9f8dac69
amomentforever0 <amomentforever0@gmail.com>
2019-09-09 22:50:26 +0200

    merge sort implementation

--- Algorithms/Sorting/MergeSort.js
+++ Algorithms/Sorting/MergeSort.js
@@ -14,7 +14,27 @@ const mergeSort = (array) => {
     console.log('left: ', left);
     console.log('right: ', right);
 
-    return mergeSort(mergeSort(left), mergeSort(right));  
+    return merge(mergeSort(left), mergeSort(right));  
+};
+
+const merge = (left, right) => {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while(leftIndex < left.length && rightIndex < right.length) {
+        if(left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
 };
 
 console.log(mergeSort(samples.unsortedArray));
